30990af2f7b0c02deec3acf18eb2dfec6cc7953e
Andy Rudnitsky <andy.rudnitsky@gmail.com>
2022-03-03 23:14:41 -0500

    Fixes

--- backup-pi-directory.sh
+++ backup-pi-directory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IPADDR=$(hostname -I | sed 's/.$//')
+IPADDR=$(hostname)
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 TARDIR='/home/pi/backups'
 TARFILE="${TARDIR}/home-pi_${IPADDR}_${TIMESTAMP}.tar.gz"
@@ -12,6 +12,5 @@ cd /home
 
 [ ! -d ${TARDIR} ] && mkdir ${TARDIR}
 rm -f ${TARDIR}/*.tar.gz
-touch ${TARFILE}
-tar --exclude=${TARDIR} -czf ${TARFILE} pi
+tar --exclude=$TARFILE -czf $TARFILE pi
 echo "Created $(ls ${TARFILE})"
